cbc755cf9d363a3ab94eff4f9243babac03e8c31
Fokko <24793348+FokkoVeegens@users.noreply.github.com>
2022-02-15 09:53:43 +0100

    Added IsDeleted column

--- get-azure-artifacts-feeds-sizes.sql
+++ get-azure-artifacts-feeds-sizes.sql
@@ -3,12 +3,17 @@
 --   * Get Team Project info along with the other data
 --   * Rounded the size to 1 decimal
 --   * Added column names
+--   * Added column to indicate if a feed has been deleted
 
 -- First part retrieves storage usage per feed
 
 SELECT ISNULL(proj.project_name, '**Collection Feed**') TeamProject,
        f.FeedName Feed,
-       FORMAT(ROUND(SUM(CAST(list.BlockFileLength AS DECIMAL(38)))/1024.0/1024.0, 1), '0.0') SizeInMb
+       FORMAT(ROUND(SUM(CAST(list.BlockFileLength AS DECIMAL(38)))/1024.0/1024.0, 1), '0.0') SizeInMb,
+       CASE 
+         WHEN f.DeletedDate IS NULL THEN 'FALSE' 
+	 ELSE 'TRUE' 
+       END IsDeleted
 FROM BlobStore.tbl_Blob blob
    INNER JOIN BlobStore.tbl_BlockList list ON list.BlobId = blob.BlobId
    INNER JOIN Feed.tbl_PackageVersionIndex fd ON '0x'+fd.StorageId = CONVERT(VARCHAR(MAX),blob.BlobId ,1) 
@@ -17,7 +22,8 @@ FROM BlobStore.tbl_Blob blob
    LEFT OUTER JOIN dbo.tbl_Dataspace d ON f.DataspaceId = d.DataspaceId
    LEFT OUTER JOIN dbo.tbl_Projects proj ON d.DataspaceIdentifier = proj.project_id
 GROUP BY proj.project_name,
-         f.FeedName
+         f.FeedName,
+	 CASE WHEN f.DeletedDate IS NULL THEN 'FALSE' ELSE 'TRUE' END
 ORDER BY SizeInMb DESC
 
 -- Second part retrieves storage usage per individual package
